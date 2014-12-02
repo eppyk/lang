e5550b10458d8b4cf59ebf8db63f980558bb64cb
Kinga Kieczkowska <k.kieczkowska@gmail.com>
2014-12-02 02:19:08 +0100

    added the answer scoring feature

--- frenchtest.py
+++ frenchtest.py
@@ -13,37 +13,51 @@ def append(b, c):
 
     file.close()
 
-def test(d):
+def test():
+    x = 0
+    y = 0
+    z = "1"
+    while z == "1":
+        d = raw_input("Do you want to be asked in Eng or French?\n")
 
-#file = open("wordbank.py", "r")
-    if(d=="1"):
-        print "Please type the French translation of: "
-        f=open('wordbank.py')
-        e = file_len('wordbank.py')
-        g = random.randrange(1, e+1, 2)
-        lines=f.readlines()
-        print lines[g]
-        h = raw_input() + "\n"
-        if(h == lines[g-1]):
-            print "Good job!"
+        if d=="Eng":
+            print "Please type the French translation of: "
+            f=open('wordbank.py')
+            e = file_len('wordbank.py')
+            g = random.randrange(1, e+1, 2)
+            lines=f.readlines()
+            print lines[g]
+            h = raw_input() + "\n"
+            if(h == lines[g-1]):
+                print "Good job!"
+                x += 1
+            else:
+                print "Oh :( Wrong. The French translation is %s" % (lines[g-1])
+                y += 1
+
+
+        elif d=="French":
+            print "Please type the English translation:"
+            f=open('wordbank.py')
+            e = file_len('wordbank.py')
+            g = random.randrange(0, e+1, 2)
+            lines=f.readlines()
+            print lines[g]
+            h = raw_input() + "\n"
+            if(h == lines[g+1]):
+                print "Good job!"
+                x += 1
+            else:
+                print "Oh :( Wrong. The French translation is %s" % (lines[g+1])
+                y += 1
         else:
-            print "Oh :( Wrong. The French translation is %s" % (lines[g-1])
+            print "wrong"
+
+        z = raw_input("Do you want to continue? \n 1 - Yes \n 2 - No, thanks\n")
 
 
-    elif(d=="2"):
-        print "Please type the English translation:"
-        f=open('wordbank.py')
-        e = file_len('wordbank.py')
-        g = random.randrange(0, e+1, 2)
-        lines=f.readlines()
-        print lines[g]
-        h = raw_input() + "\n"
-        if(h == lines[g+1]):
-            print "Good job!"
-        else:
-            print "Oh :( Wrong. The French translation is %s" % (lines[g+1])
-    else:
-        print "wrong"
+    print "Had enough? You answered %d questions and your score is:\n %d good answers \n %d wrong answers" % (x+y, x, y)
+
 
 
 a = input("Hey! What do you wanna do? \n 1 - add a new word to the bank \n 2 - test yourself \n")
@@ -59,7 +73,5 @@ if (a==1):
 
     append (b, c)
 
-else:
-    d = raw_input("Do you want to be asked in Eng or French? \n 1 - English \n 2 - French\n")
-
-    test(d)
+elif a==2:
+    test()
